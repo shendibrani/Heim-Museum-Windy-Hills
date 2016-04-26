671cc906dfaf89a4ea9a55556762c52fd215849c
Giulio Robecchi <giulio.robecchi@gmail.com>
2016-04-26 14:19:01 +0200

    Changed and completed heim hud code

--- Assets/Heim Backbone/HeimHUD.cs
+++ Assets/Heim Backbone/HeimHUD.cs
@@ -1,12 +1,20 @@
+using UnityEngine;
+using System.Collections;
+
+public class HeimHUD {
+
 	private Texture2D[] hudAnimationArray = new Texture2D[40];
 	private Texture2D HUDtop;
 	private string temp;
+	private int arrayPosition;
 
-	void OnGUI() {		
+	void OnGUI() 
+	{		
 		AnimateHudTop("HUD/Hudcrop_");
 	}
 	
-	public void AnimateHudTop(string imageName) {
+	public void AnimateHudTop(string imageName) 
+	{
 		AssignHUDTextures(imageName);
 		
 		if(arrayPosition < hudAnimationArray.Length) {
@@ -24,17 +32,10 @@
 		GUI.DrawTexture(new Rect(((Screen.width / 2)-(1150 / 2) - 20),-10,1150,150),HUDtop);
 	}
 	
-	private void AssignHUDTextures(string imageName) {
+	private void AssignHUDTextures(string imageName) 
+	{
 		for(int i = 0; i < hudAnimationArray.Length; i++) {
-			if(i < 10) {
-				temp = "0000" + i.ToString();
-			}
-			else if(i >= 10 && i <= 99) {
-				temp = "000" + i.ToString();
-			}
-			else if(i >= 100) {
-				temp = "00" + i.ToString();
-			}
-			hudAnimationArray[i] = (Texture2D)Resources.Load(imageName + temp, typeof(Texture2D));	
+			hudAnimationArray[i] = (Texture2D)Resources.Load(imageName + i.ToString("D5"), typeof(Texture2D));	
 		}
 	}
+}
