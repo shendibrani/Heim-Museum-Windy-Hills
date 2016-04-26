9bf744dae3dac39829e7dbff7bc297847d321549
Giulio Robecchi <giulio.robecchi@gmail.com>
2016-04-26 11:51:50 +0200

    Created place-on-click behaviour for prototype

---
+++ Assets/Scripts/PlaceObjectOnClick.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaceObjectOnClick : MonoBehaviour {
+
+	[SerializeField] GameObject prefab;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseOver()
+	{
+		if(Input.GetMouseButtonDown(0)){
+			RaycastHit hit;
+			Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit);
+			if(hit.collider.gameObject == this.gameObject){
+				GameObject instance = (GameObject) GameObject.Instantiate(prefab, hit.point, Quaternion.identity);
+			}
+		}
+	}
+}
