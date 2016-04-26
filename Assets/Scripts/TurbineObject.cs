de70cb59f3a4f619db568e7ca20aaed665728432
TemplarWarden <daniel.louis.raynor@gmail.com>
2016-04-26 13:50:24 +0200

    Stuff Added, Some Backbone stuff, and TurbineObject Script

---
+++ Assets/Scripts/TurbineObject.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurbineObject : MonoBehaviour {
+
+    //Wind Direction in Vector
+    public static Vector3 windDirection;
+
+    //Maximum Power Avalible from Turbine (in MW)
+    float _maxPower = 1;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        WindDetectionRaycast();
+	}
+
+    //Raycast to the this object according to wind direction. If an object is in the way, the efficency descreases depending on objects in the way
+    void WindDetectionRaycast()
+    {
+        Ray obstructionRay = new Ray(transform.position, -windDirection);
+        RaycastHit [] hitObjects;
+        hitObjects = Physics.RaycastAll(obstructionRay);
+
+
+        float efficency = 1;
+        foreach (RaycastHit hit in hitObjects)
+        {
+            if (hit.collider != GetComponent<Collider>())
+            {
+                efficency -= 0.25f;
+            }
+        }
+
+    }
+}
