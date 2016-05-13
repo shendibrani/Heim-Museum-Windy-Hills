4cc061277e0af00410c544510a5ed69b59dbeda5
Giulio Robecchi <giulio.robecchi@gmail.com>
2016-05-13 13:02:56 +0200

    Added mouse sensitive, fixed version of the scene, added mousemanager

---
+++ Assets/Scripts/IMouseSensitive.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using UnityEngine;
+
+public interface IMouseSensitive
+{
+	void OnClick(RaycastHit hit);
+}
+
---
+++ Assets/Scripts/MouseManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseManager : MonoBehaviour {
+
+	[SerializeField] bool mouseInput;
+
+	// Update is called once per frame
+	void Update () {
+		if(mouseInput){
+			if(Input.GetMouseButtonDown(0)){
+				RaycastHit hit;
+				Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit);
+				if(hit.collider != null && hit.collider.GetComponent<IMouseSensitive>() != null){
+					hit.collider.gameObject.GetComponent<IMouseSensitive>().OnClick (hit);
+				}
+			}
+		}
+	}
+}
