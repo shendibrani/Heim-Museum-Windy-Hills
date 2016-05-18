1f8879d051e41b0c087cdae45780697ae89062b5
Shend Ibrani <shend_ibrani@hotmail.com>
2016-05-18 22:29:55 +0200

    set up the stage for events

---
+++ Assets/Scripts/EventScripts/EventHandler.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class EventHandler : MonoBehaviour {
+
+    StormCloudEvent stormCloud;
+    public List<EventsClass> eventsList;
+
+	// Use this for initialization
+	void Start () {
+        eventsList.Add(stormCloud);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
