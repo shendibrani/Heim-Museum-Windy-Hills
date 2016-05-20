8cf0cb65cd90cc1cd2111f3c1db802ff36c4232f
Shend Ibrani <shend_ibrani@hotmail.com>
2016-05-20 11:36:30 +0200

    Stormy weather time!

--- Assets/Scripts/EventScripts/EventHandler.cs
+++ Assets/Scripts/EventScripts/EventHandler.cs
@@ -3,24 +3,30 @@ using System.Collections.Generic;
 
 public class EventHandler : MonoBehaviour {
 
-    StormCloudEvent stormCloud;
+    StormCloudEvent stormCloudEvent;
+    GameObject stormGO;
+
+
     private List<EventsClass> eventsList;
     static public int rnd;
     // Use this for initialization
     void Start () {
+        stormGO = (GameObject)Resources.Load("StormCloud");
+
         eventsList = new List<EventsClass>();
-        eventsList.Add(stormCloud);
+        eventsList.Add(stormCloudEvent);
 	}
 
-    void InitializeEvent<T>() where T : EventsClass
+    void InitializeEvent<T>() where T : StormCloudEvent 
     {
         rnd = Random.Range(0, TurbineObject.all.Count);
-        TurbineObject.all[rnd].gameObject.AddComponent<T>();
+        GameObject tempStorm = Instantiate(stormGO);
+        tempStorm.gameObject.AddComponent<StormCloudEvent>() ;
     }
 
     bool initializedEvent = false;
     // Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
         if (TurbineObject.all.Count > 0 && !initializedEvent)
         {
             InitializeEvent<StormCloudEvent>();
--- Assets/Scripts/EventScripts/StormCloudEvent.cs
+++ Assets/Scripts/EventScripts/StormCloudEvent.cs
@@ -3,86 +3,64 @@ using System.Collections.Generic;
 using System;
 using System.Diagnostics;
 
-public class StormCloudEvent : EventsClass {
+public class StormCloudEvent : EventsClass, IWindSensitive {
 
     public float Speed;
-    private GameObject stormPrefab;
-    private GameObject tempStorm;
+    //private GameObject stormPrefab;
+    //private GameObject tempStorm;
     private TurbineObject selectedTurbine;
     private Vector3 turbinePos;
-    private Vector3 stormTargetPosition;
-
-    Stopwatch Timer;
-  
+    private Vector3 stormSpawnPos;
 
     // Use this for initialization
     void Start () {
-        Speed = 100.0f;
-        stormPrefab = (GameObject)Resources.Load("StormCloud");
+        
+      //  stormPrefab = (GameObject)Resources.Load("StormCloud");
         EventStart();
     }
 	
 	// Update is called once per frame
 	void Update () {
-        
-        
-        MoveCloudToTurbine();
-        startStormFunctions();
+        if (TurbineObject.all.Count > 0) {
+            
+            MoveCloud();
+            if (EnteredWindzone) transform.Translate(5, 0, 5);
+
+        }
     }
 
     public override void EventStart()
     {
-
-        selectedTurbine = TurbineObject.all[EventHandler.rnd];
-        turbinePos = selectedTurbine.transform.position;
-        stormTargetPosition = new Vector3(turbinePos.x, turbinePos.y + 30, turbinePos.z);
-        tempStorm = (GameObject)Instantiate(stormPrefab, turbinePos + new Vector3(600, 30, 0), Quaternion.identity);
-
+        transform.position = new Vector3(UnityEngine.Random.Range(500.0f, 700.0f), 60.0f, UnityEngine.Random.Range(50.0f, 300.0f));
+       // tempStorm = (GameObject)Instantiate(stormPrefab, stormSpawnPos, Quaternion.identity);
     }
 
-    void MoveCloudToTurbine() {
-        if (tempStorm != null)
-        tempStorm.transform.position = Vector3.MoveTowards(tempStorm.transform.position, stormTargetPosition , Speed * Time.deltaTime);
-    }
-
-    void DestroyAfterTime(int seconds) {
-        if (Timer.Elapsed.Seconds > seconds)
-        {
-            Timer.Stop();
-            UnityEngine.Debug.Log("Destroyed StormCloud");
-            Destroy(tempStorm);
-        }
-        
-    }
-
-    bool StormPassedScreen() {
-       
-            if (tempStorm != null && tempStorm.transform.position == stormTargetPosition)
-            {
-                UnityEngine.Debug.Log("storm is above turbine!");
-                return true;
-           }
-        else return false;
-    }
-
-    bool timerStart = false;
-    private void startStormFunctions() {
-        if (StormPassedScreen())
-        {
-            if (!timerStart)
-            {
-                Timer = Stopwatch.StartNew();
-                timerStart = true;
-            }
-
-            UnityEngine.Debug.Log("Stopwatch: " + Timer.Elapsed.Seconds + " second(s)");
-            DestroyAfterTime(10);
-        }
+    void MoveCloud() {
+        if (this != null)
+            transform.Translate(-5, 0, 0);
     }
 
     public override void EventEnd()
     {
        
     }
-   
+
+    void OnTriggerEnter(Collider col) {
+        if (col.gameObject.tag == "DestroyerOfClouds") {
+            Destroy(this.gameObject);
+        }
+
+    }
+
+    bool EnteredWindzone = false;
+    public void OnEnterWindzone()
+    {
+        EnteredWindzone = true;
+        
+    }
+
+    public void OnExitWindzone()
+    {
+        EnteredWindzone = false;
+    }
 }
