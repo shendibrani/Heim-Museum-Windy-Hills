9005e29a07c4f6901f628fce8eff8c6ee8076662
Benita Stärke <benitastaerke987@gmail.com>
2016-05-20 11:36:03 +0200

    wind particle

---
+++ Assets/Scripts/WindParticleControll.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WindParticleControll : MonoBehaviour {
+
+	[SerializeField] ParticleSystem Wind;
+
+	[SerializeField] [Range(0,1)] float Strength = 0;
+
+	[SerializeField] float BaseSize = 2;
+	[SerializeField] float BaseLifetime = 2;
+
+	void Start ()
+	{
+
+	}
+
+
+	void Update ()
+	{
+		Wind.startSize = Random.Range (0.1f, Mathf.Lerp(0.1f,BaseSize,Strength));
+		Wind.startLifetime = Random.Range (1,Mathf.Lerp(1,BaseLifetime,Strength));
+	}
+}
