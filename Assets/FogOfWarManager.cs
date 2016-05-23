aaaac9382300b82fa40cd99626e9990ed534eacd
Giulio Robecchi <giulio.robecchi@gmail.com>
2016-05-23 12:29:52 +0200

    Fog of war basic setup.

---
+++ Assets/FogOfWarManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class FogOfWarManager : MonoBehaviour {
+
+	HashSet<FogOfWarPiece> pieces;
+
+	// Use this for initialization
+	void Start () {
+		pieces = new HashSet<FogOfWarPiece>(GetComponentsInChildren<FogOfWarPiece>());
+	}
+
+	public void ShowArea(Vector2 center, float radius)
+	{
+		foreach (FogOfWarPiece p in pieces){
+			if(Vector2.Distance(p.transform.position, center) < radius){
+				p.MoveAway(center, radius);
+			}
+		}
+	}
+}
---
+++ Assets/FogOfWarPiece.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(RectTransform))]
+public class FogOfWarPiece : MonoBehaviour {
+
+	Vector2 originalPosition;
+
+	Vector2 currentTargetPositon;
+
+	// Use this for initialization
+	void Start () 
+	{
+		originalPosition = GetComponent<RectTransform>().anchoredPosition;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		GetComponent<RectTransform>().anchoredPosition = Vector2.Lerp(GetComponent<RectTransform>().anchoredPosition, currentTargetPositon, 0.5f);
+	}
+
+	public void MoveAway(Vector2 point, float radius)
+	{
+		Vector2 direction = (GetComponent<RectTransform>().anchoredPosition - point).normalized;
+
+		currentTargetPositon = originalPosition + (direction * radius);
+	}
+
+	public void MoveBack()
+	{
+		currentTargetPositon = originalPosition;
+	}
+}
