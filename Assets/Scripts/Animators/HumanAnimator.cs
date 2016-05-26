5bb3f084bc597063bf2246dfc1ec90d650c15208
Benita Stärke <benitastaerke987@gmail.com>
2016-05-26 11:49:11 +0200

    Human Animator

---
+++ Assets/Scripts/Animators/HumanAnimator.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HumanAnimator : MonoBehaviour {
+
+	enum Type
+	{
+		Farmer,
+		Saboteur,
+	}
+
+	Animator _anim;
+
+	[SerializeField] Type type = Type.Farmer;
+
+	void Start ()
+	{
+		_anim = GetComponent<Animator> ();
+
+		if (type == Type.Farmer)
+		{
+			_anim.SetInteger ("Type", 0);
+		}
+		else if (type == Type.Saboteur)
+		{
+			_anim.SetInteger ("Type", 1);
+		}
+	}
+
+	public void SetClimb(bool climb)
+	{
+		_anim.SetBool ("Climbing", climb);
+	}
+}
