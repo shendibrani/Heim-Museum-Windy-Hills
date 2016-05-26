e8c9f6e2abfa6380cac55f95cf16396d6dcfc457
Benita Stärke <benitastaerke987@gmail.com>
2016-05-26 19:48:13 +0200

    PigeonAwesomeess

--- Assets/Scripts/Animators/PigeonAnimator.cs
+++ Assets/Scripts/Animators/PigeonAnimator.cs
@@ -4,12 +4,19 @@ using System.Collections;
 public class PigeonAnimator : MonoBehaviour {
 
 	Animator _anim;
+	Vector3 _goal;
 
 	// Use this for initialization
 	void Start ()
 	{
 		_anim = GetComponent<Animator> ();
-		_anim.SetFloat("SpeedRandom",Random.Range(0.5f,1.5f));
+		RandomVal ();
+	}
+
+	void Update()
+	{
+		
+		transform.localPosition = Vector3.Lerp (transform.localPosition, _goal, 0.001f);
 	}
 
 	public void Flap()
@@ -23,5 +30,13 @@ public class PigeonAnimator : MonoBehaviour {
 		{
 			_anim.SetBool ("Flap", false);
 		}
+		RandomVal ();
+	}
+
+	void RandomVal()
+	{
+		_anim.SetFloat("SpeedRandom",Random.Range(0.5f,1.3f));
+		_goal = new Vector3 (Random.Range (-2, 2), 0, Random.Range (-2, 2));
+		Debug.Log (_goal);
 	}
 }
