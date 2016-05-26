fdd7093dd573ec3823f00d0afdceded38584bf75
Benita Stärke <benitastaerke987@gmail.com>
2016-05-26 12:59:02 +0200

    PIGEON ATTACK

---
+++ Assets/Scripts/Animators/PigeonAnimator.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PigeonAnimator : MonoBehaviour {
+
+	Animator _anim;
+
+	// Use this for initialization
+	void Start ()
+	{
+		_anim = GetComponent<Animator> ();
+		_anim.SetFloat("SpeedRandom",Random.Range(0.5f,1.5f));
+	}
+
+	public void Flap()
+	{
+		if (_anim.GetBool("Flap") == false)
+		{
+			_anim.SetBool ("Flap", true);
+		}
+
+		else
+		{
+			_anim.SetBool ("Flap", false);
+		}
+	}
+}
