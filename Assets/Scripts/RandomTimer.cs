b2495645ef6243cd40c95cf71550316ce8c07d9f
Giulio Robecchi <giulio.robecchi@gmail.com>
2016-05-26 13:19:34 +0200

    Added random flapping for pigeons

---
+++ Assets/Scripts/RandomTimer.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using System.Collections;
+
+public class RandomTimer : MonoBehaviour {
+
+	float timer;
+
+	[SerializeField] int minSeconds, maxSeconds;
+
+	[SerializeField] UnityEvent TimerEnd;
+
+	// Use this for initialization
+	void Start () {
+		timer = (RNG.NextFloat() * RNG.Next(0,maxSeconds-minSeconds)) + minSeconds;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer -= Time.deltaTime;
+
+		if(timer <= 0){
+			TimerEnd.Invoke();
+			timer = (RNG.NextFloat() * RNG.Next(0,maxSeconds-minSeconds)) + minSeconds;
+		}
+	}
+}
