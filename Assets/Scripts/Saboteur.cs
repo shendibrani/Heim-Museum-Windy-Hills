ea844b03da16bce016de06d022bf0b120e8f0361
Benita Stärke <benitastaerke987@gmail.com>
2016-05-30 14:54:27 +0200

    new saboteur animation

--- Assets/Scripts/Saboteur.cs
+++ Assets/Scripts/Saboteur.cs
@@ -3,35 +3,61 @@ using System.Collections;
 
 public class Saboteur : MonoBehaviour {
 
-	Animator _anim;
 	Vector3 _startPosition;
 	float _end = 19.95f;
 
- 	[SerializeField] float Speed = 0.01f;
+ 	[SerializeField] float Speed = 0f;
+	[SerializeField] Animator Anim;
 
-	float Progress = 0;
+	float _progress = 0;
+	bool _isDone = false;
 
 	void Start()
 	{
 		_startPosition = transform.localPosition;
+		Anim.SetInteger ("Type", 1);
 	}
 
 	void Update ()
 	{
-		// += speed is for testingl use the public Progress for final verison
-		Progress += Speed;
-		transform.localPosition = _startPosition + Mathf.Lerp(_startPosition.y,_end,Progress) * Vector3.up;
+		if (_progress > 1)
+		{
+			Anim.SetBool ("Succes", true);
+			Speed = 0;
+			_progress = 0;
+
+			if (! _isDone)
+			{
+				transform.localPosition = new Vector3(0,29.74f,0);
+				transform.localRotation = Quaternion.identity;
+				_isDone = true;
+			}
+		}
+		else if (_progress < 0)
+		{
+			Speed = 0;
+			_progress = 0;
+			gameObject.SetActive(false);
+		}
+		else
+		{
+			_progress += Speed;
+			transform.localPosition = _startPosition + Mathf.Lerp(_startPosition.y,_end,_progress) * Vector3.up;
+		}
 	}
 
 	public void StartAnimation()
 	{
-		Progress = 0;
+		_progress = 0;
 		transform.localPosition = _startPosition;
 		gameObject.SetActive(true);
+		Anim.SetBool ("Climbing", true);
+		Speed = 0.001f;
 	}
 
 	public void EndAnimation()
 	{
-		gameObject.SetActive(false);
+		Anim.SetBool ("Defeated", true);
+		Speed = -0.01f;
 	}
 }
