7e018a77e1cc5c0a6e05463c92914755421bab2a
Shend Ibrani <shend_ibrani@hotmail.com>
2016-05-31 20:08:47 +0200

    polished flock storm, needs more polishing

--- Assets/Scripts/EventScripts/StormBehavior.cs
+++ Assets/Scripts/EventScripts/StormBehavior.cs
@@ -4,12 +4,12 @@ using System.Collections;
 public class StormBehavior : MonoBehaviour, IWindSensitive
 {
 
-    float WindRadius = 40.0f;
-    float DamageRadius =30.0f;
+    float WindRadius = 30.0f;
+    float DamageRadius = 12.0f;
 
-	public float speed;
+    public float speed;
 
-    bool debug = true;
+    public bool debug = true;
     // Update is called once per frame
     void Update()
     {
@@ -23,10 +23,19 @@ public class StormBehavior : MonoBehaviour, IWindSensitive
     void WindEffect() {
         foreach (TurbineObject to in TurbineObject.all) {
             float distance = Vector3.Distance(new Vector3(to.transform.position.x, 0, transform.position.z), new Vector3(this.transform.position.x, 0, this.transform.position.z));
-
-            if (distance <= WindRadius) to.IncreaseEfficiency();
-            if (distance <= DamageRadius) to.BreakTurbine();
+            Debug.Log("Distance: " + distance);
+            if (distance <= WindRadius) {
+                to.IncreaseEfficiency();
+                Debug.Log("Increasing Efficiency by stormcloud!");
+            }
+            if (distance <= DamageRadius) {
+                to.BreakTurbine(); Debug.Log("Stormcloud instantly destroyed windmill");
+            }
         }
+    }
+
+    void CollideWithTurbine() {
+     
 
     }
 
