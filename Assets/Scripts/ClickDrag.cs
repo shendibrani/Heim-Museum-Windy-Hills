22c5c3950c61059d57baf8f4f097be3c0c1707e7
Giulio Robecchi <giulio.robecchi@gmail.com>
2016-06-01 16:45:46 +0200

    Added Click and drag system.

---
+++ Assets/Scripts/ClickDrag.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using System.Collections;
+
+public class ClickDrag : MonoBehaviour {
+
+	int currentTouchID;
+	bool dragged;
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+
+	public void OnDrag(UnityEngine.EventSystems.BaseEventData data)
+	{
+		Debug.Log ("Callback");
+
+		var pointerEventData = data as UnityEngine.EventSystems.PointerEventData;
+
+		if (pointerEventData == null) { 
+			Debug.Log ("Wrong Type of Event"); 
+			return;
+		}
+			
+		GetComponent<RectTransform>().anchoredPosition += pointerEventData.delta/GetComponentInParent<Canvas>().scaleFactor;
+	}
+	public void OnDragEnd()
+	{
+
+	}
+}
