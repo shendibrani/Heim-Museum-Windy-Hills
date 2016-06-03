f5c8452faf0e90ca6cf092bcb4fa46239b4343e3
Shend Ibrani <shend_ibrani@hotmail.com>
2016-06-03 13:35:39 +0200

    COMMITMENT

--- Assets/Scripts/EventScripts/FlockBehavior.cs
+++ Assets/Scripts/EventScripts/FlockBehavior.cs
@@ -6,7 +6,7 @@ using System.Diagnostics;
 public class FlockBehavior : MonoBehaviour
 {
 
-    float WindRadius = 60.0f;
+    
     float DamageRadius = 27.0f;
 
     Stopwatch timer;
@@ -30,9 +30,7 @@ public class FlockBehavior : MonoBehaviour
         if (TurbineObject.all.Count > 0)
         {
             MoveToWindMill();
-            WindEffect();
-            
-           
+            WindEffect();    
         }
     }
 
@@ -40,42 +38,31 @@ public class FlockBehavior : MonoBehaviour
     void WindEffect()
     {
         float distance = Vector3.Distance(new Vector3(selectedTurbined.transform.position.x, 30, transform.position.z), this.transform.position);
-        if (distance <= DamageRadius & effectApplied == false) {
+        if (distance < DamageRadius && effectApplied == false) {
             selectedTurbined.BreakTurbine();
             effectApplied = true;
             timer.Start();
-            
+            transform.LookAt(selectedTurbined.transform);
         }
-
     }
+
     bool moveToWindmill = true;
 
     void MoveToWindMill()
     {
-        if (moveToWindmill) { 
-            Transform target = TurbineObject.all[randomed].transform;
-            transform.position = Vector3.MoveTowards(transform.position, new Vector3(target.position.x, target.position.y + 30, target.position.z), 1.5f);
-            transform.LookAt(target);
+        if (moveToWindmill && !effectApplied) { 
+            
+            transform.position = Vector3.MoveTowards(transform.position, new Vector3(selectedTurbined.transform.position.x, selectedTurbined.transform.position.y + 30, selectedTurbined.transform.position.z), 1.5f);
+            transform.LookAt(selectedTurbined.transform);
         }
         if (timer.Elapsed.Seconds > 3) moveToWindmill = false;
         MoveAway();
     }
 
     void MoveAway() {
-        if (!moveToWindmill) {
+        if (!moveToWindmill && effectApplied) {
             transform.position = Vector3.MoveTowards(transform.position, new Vector3(transform.position.x - 300, transform.position.y, transform.position.z), 1.5f);
             if (transform.position.x < -20) Destroy(gameObject);
-        }
-
-    }
-
-    void OnDrawGizmos()
-    {
-        if (debug)
-        {
-            Gizmos.color = Color.blue;
-            Gizmos.DrawSphere(transform.position, WindRadius);
-            Gizmos.DrawSphere(transform.position, DamageRadius);
         }
     }
 
