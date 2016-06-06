8b9f77c7259ffe15434a0d1e48af53ff0b9531ef
Benita Stärke <benitastaerke987@gmail.com>
2016-06-06 10:17:13 +0200

    Missing asets returned from their long long journey

---
+++ Assets/Game Assets/Particle Assets/Fire/FireScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireScript : MonoBehaviour {
+
+    ParticleSystem fire;
+    public GameObject smokeObject;
+    ParticleSystem smoke;
+    float lifetime = 1.0f;
+    float smokeLifetime = 12.0f;
+    public bool burning = false;
+
+	void Start () {
+        fire = GetComponent<ParticleSystem>();
+        smoke = smokeObject.GetComponent<ParticleSystem>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	if (burning == true)
+        {
+            if (lifetime < 4.0f)
+            {
+                lifetime += 0.009f;
+                smokeLifetime += 0.025f;
+                fire.GetComponent<ParticleSystem>().startLifetime = lifetime;
+                smoke.GetComponent<ParticleSystem>().startLifetime = smokeLifetime;
+            }
+
+
+        }
+	}
+
+
+}
