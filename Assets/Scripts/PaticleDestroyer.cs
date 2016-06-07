3496f7abc1eeccede8527504732817fcd3ad41ab
Benita Stärke <benitastaerke987@gmail.com>
2016-06-07 23:57:44 +0200

    made particles delete themselves and stuff happened

---
+++ Assets/Scripts/PaticleDestroyer.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PaticleDestroyer : MonoBehaviour {
+
+	void Update ()
+	{
+		if (!GetComponent<ParticleSystem>().isPlaying)
+		{
+			Destroy (this.gameObject);
+		}
+	}
+}
