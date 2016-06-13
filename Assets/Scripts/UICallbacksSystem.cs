cd5ce925041345ec65037ee339dafaa9b992fb04
Giulio Robecchi <giulio.robecchi@gmail.com>
2016-06-13 14:46:29 +0200

    Added highlighting buttons.

--- Assets/Scripts/UICallbacksSystem.cs
+++ Assets/Scripts/UICallbacksSystem.cs
@@ -5,20 +5,44 @@ public class UICallbacksSystem : MonoBehaviour
 {
 	public static UIState currentState;
 
-	public void OnPolice(){
+	[SerializeField] Highlightable police, firemen, repair, cleanup;
+
+	public void OnPolice()
+	{
+		Clear ();
+		police.SetHighlight(true);
 		currentState = UIState.Police;
 	}
 
-	public void OnFiremen(){
+	public void OnFiremen()
+	{
+		Clear ();
+		firemen.SetHighlight(true);
 		currentState = UIState.Firemen;
 	}
 
-	public void OnRepair(){
+	public void OnRepair()
+	{
+		Clear ();
+		repair.SetHighlight(true);
 		currentState = UIState.Repair;
 	}
 
-	public void OnCleanup(){
+	public void OnCleanup()
+	{
+		Clear ();
+		cleanup.SetHighlight(true);
 		currentState = UIState.Cleanup;
+	}
+
+	public void Clear()
+	{
+		currentState = UIState.None;
+
+		police.SetHighlight (false);
+		firemen.SetHighlight (false);
+		repair.SetHighlight (false);
+		cleanup.SetHighlight (false);
 	}
 }
 
