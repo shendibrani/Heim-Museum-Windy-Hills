c5a8058f76cfa9f617056e6baf6f2c262d2d00f1
Giulio Robecchi <giulio.robecchi@gmail.com>
2016-06-13 14:25:06 +0200

    switched from click drag to click click system

---
+++ Assets/Scripts/UICallbacksSystem.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UICallbacksSystem : MonoBehaviour 
+{
+	public static UIState currentState;
+
+	public void OnPolice(){
+		currentState = UIState.Police;
+	}
+
+	public void OnFiremen(){
+		currentState = UIState.Firemen;
+	}
+
+	public void OnRepair(){
+		currentState = UIState.Repair;
+	}
+
+	public void OnCleanup(){
+		currentState = UIState.Cleanup;
+	}
+}
+
+public enum UIState 
+{
+	None, Police, Firemen, Repair, Cleanup
+}
