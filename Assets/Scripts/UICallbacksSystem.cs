2e49442d6d595de97208668da08291a7e5c340b5
TemplarWarden <daniel.louis.raynor@gmail.com>
2016-06-15 16:40:10 +0200

    Fixed placing on bad spots. Buttons now can toggle on/off

--- Assets/Scripts/UICallbacksSystem.cs
+++ Assets/Scripts/UICallbacksSystem.cs
@@ -1,52 +1,74 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class UICallbacksSystem : MonoBehaviour 
+public class UICallbacksSystem : MonoBehaviour
 {
-	public static UIState currentState;
+    public static UIState currentState;
 
-	[SerializeField] Highlightable police, firemen, repair, cleanup;
+    [SerializeField]
+    Highlightable police, firemen, repair, cleanup;
 
-	public void OnPolice()
-	{
-		Clear ();
-		police.SetHighlight(true);
-		currentState = UIState.Police;
-	}
+    public void OnPolice()
+    {
+        if (ClearCheck(UIState.Police))
+        {
+            police.SetHighlight(true);
+            currentState = UIState.Police;
+        }
+    }
 
-	public void OnFiremen()
-	{
-		Clear ();
-		firemen.SetHighlight(true);
-		currentState = UIState.Firemen;
-	}
+    public void OnFiremen()
+    {
+        if (ClearCheck(UIState.Firemen))
+        {
+            firemen.SetHighlight(true);
+            currentState = UIState.Firemen;
+        }
+    }
 
-	public void OnRepair()
-	{
-		Clear ();
-		repair.SetHighlight(true);
-		currentState = UIState.Repair;
-	}
+    public void OnRepair()
+    {
+        if (ClearCheck(UIState.Repair))
+        {
+            repair.SetHighlight(true);
+            currentState = UIState.Repair;
+        }
+    }
 
-	public void OnCleanup()
-	{
-		Clear ();
-		cleanup.SetHighlight(true);
-		currentState = UIState.Cleanup;
-	}
+    public void OnCleanup()
+    {
+        if (ClearCheck(UIState.Cleanup))
+        {
+            cleanup.SetHighlight(true);
+            currentState = UIState.Cleanup;
+        }
+    }
 
-	public void Clear()
-	{
-		currentState = UIState.None;
+    public void Clear()
+    {
+        currentState = UIState.None;
 
-		police.SetHighlight (false);
-		firemen.SetHighlight (false);
-		repair.SetHighlight (false);
-		cleanup.SetHighlight (false);
-	}
+        police.SetHighlight(false);
+        firemen.SetHighlight(false);
+        repair.SetHighlight(false);
+        cleanup.SetHighlight(false);
+    }
+
+    public bool ClearCheck(UIState state)
+    {
+        if (currentState == state)
+        {
+            Clear();
+            return false;
+        }
+        else {
+            Clear();
+            return true;
+        }
+    }
 }
 
-public enum UIState 
+public enum UIState
 {
-	None, Police, Firemen, Repair, Cleanup
+    None, Police, Firemen, Repair, Cleanup
 }
