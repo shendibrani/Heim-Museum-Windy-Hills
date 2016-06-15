dc37fbb6d761913d26b9f140c681a592cc44bcbd
Benita Stärke <benitastaerke987@gmail.com>
2016-06-15 12:56:14 +0200

    Wow, so things, such many

---
+++ Assets/Scripts/SunTest.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SunTest : MonoBehaviour {
+
+	[SerializeField] float speed = 0;
+
+	float dist;
+
+	// Use this for initialization
+	void Start () {
+		dist = GetComponent<Light> ().cookieSize;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (this.transform.localPosition.x < dist)
+		{
+			this.transform.localPosition += new Vector3 (speed, 0, 0);
+		} else {
+			this.transform.position = Vector3.zero;
+		}
+	}
+}
