42c7bb8c1c8904a1fe084802a16f3706ca087985
Giulio Robecchi <giulio.robecchi@gmail.com>
2016-06-20 13:39:48 +0200

    Added generic dispatcher system.

---
+++ Assets/Scripts/Dispatcher and Messages.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Message
+{
+	public readonly GameObject Sender;
+
+	public Message(GameObject sender) 
+	{
+		Sender = sender;
+	}
+}
+
+public class Dispatcher<T> where T: Message
+{
+    public delegate void DispatcherCallback (T instance);
+
+    static DispatcherCallback Callback;
+
+    public static void Dispatch(T instance)
+    {
+        Callback(instance);
+    }
+
+    public static void Subscribe(DispatcherCallback d)
+    {
+        Callback += d;
+    }
+
+    public static void Unsubscribe (DispatcherCallback d)
+    {
+        Callback -= d;
+    }
+}
+
+public class FiremenMessage : Message
+{
+	public FiremenMessage (GameObject sender) : base(sender) {}
+}
+
+public class PoliceMessage : Message
+{
+	public PoliceMessage (GameObject sender) : base(sender) {}
+}
+
+public class RepairMessage : Message
+{
+	public RepairMessage (GameObject sender) : base(sender) {}
+}
+
+public class CleanupMessage : Message
+{
+	public CleanupMessage (GameObject sender) : base(sender) {}
+}
