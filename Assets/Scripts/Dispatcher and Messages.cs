be63d5d8217ae1162d358529c02645dcac7a3129
Shend Ibrani <Shend Ibrani>
2016-06-20 13:51:23 +0200

    no message

--- Assets/Scripts/Dispatcher and Messages.cs
+++ Assets/Scripts/Dispatcher and Messages.cs
@@ -20,7 +20,10 @@ public class Dispatcher<T> where T: Message
 
     public static void Dispatch(T instance)
     {
-        Callback(instance);
+        if (Callback != null)
+        {
+            Callback(instance);
+        }
     }
 
     public static void Subscribe(DispatcherCallback d)
