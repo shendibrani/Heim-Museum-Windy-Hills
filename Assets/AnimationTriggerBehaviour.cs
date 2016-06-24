e7072cda56b2b236ef07a37e52042fef6e474094
Benita Stärke <benitastaerke987@gmail.com>
2016-06-24 14:35:54 +0200

    things happened but idk

--- Assets/AnimationTriggerBehaviour.cs
+++ Assets/AnimationTriggerBehaviour.cs
@@ -3,8 +3,24 @@ using System.Collections;
 
 public class AnimationTriggerBehaviour : MonoBehaviour 
 {
+	bool wait;
+
+	void Update()
+	{
+		if (wait)
+		{
+			wait = false;
+		}
+		else
+		{
+			GetComponent<Animator>().SetBool ("Active", false);
+		}
+
+	}
+
 	public void TriggerAnimation(string name)
 	{
 		GetComponent<Animator>().SetBool (name, true);
+		wait = true;
 	}
 }
